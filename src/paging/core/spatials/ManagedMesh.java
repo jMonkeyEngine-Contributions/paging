cd034da5940a7451e8d20f8df6e704c3a37b7206
cristofflanders@gmail.com <cristofflanders@gmail.com@e932bb26-a792-46c9-b5dd-823ed31c32f9>
2012-12-18 19:28:31 +0000

    Added basic commenting

--- src/paging/core/spatials/ManagedMesh.java
+++ src/paging/core/spatials/ManagedMesh.java
@@ -13,12 +13,30 @@ import paging.core.PagingManager.LOD;
  */
 public abstract class ManagedMesh extends Mesh {
 	private LOD currentLOD;
-	
+	/**
+	 * The abstracted build method is not enforced by the paging system, instead
+	 * buildMesh() is supplied via the Delegator class.  This is for continuity's
+	 * sake by standardizing the build method of custom meshes.  Use if you like.
+	 */
 	public abstract void build();
+	/**
+	 * A mechanism used by Delegators for handling updates to LOD with the user's
+	 * custom ManagedMeshs
+	 * @param lod The PagingManager.LOD provided by the Delegator
+	 */
 	public abstract void updateLOD(LOD lod);
+	/**
+	 * Returns the current LOD of the custom mesh
+	 * @return PagingManager.LOD
+	 */
 	public LOD getCurrentLOD() {
 		return this.currentLOD;
 	}
+	/**
+	 * Ensure that this method is called by updateLOD as the Delegator's LOD management
+	 * depends on checks against the mesh's current LOD
+	 * @param lod 
+	 */
 	public void setCurrentLOD(LOD lod) {
 		this.currentLOD = lod;
 	}
