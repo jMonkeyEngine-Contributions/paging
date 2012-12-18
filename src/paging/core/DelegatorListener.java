7a7dca7c254f0be3c5a908b272626feef033fb32
cristofflanders@gmail.com <cristofflanders@gmail.com@e932bb26-a792-46c9-b5dd-823ed31c32f9>
2012-12-18 06:22:53 +0000

    Added comments

--- src/paging/core/DelegatorListener.java
+++ src/paging/core/DelegatorListener.java
@@ -11,6 +11,14 @@ import com.jme3.scene.Node;
  * @author t0neg0d
  */
 public interface DelegatorListener {
+	/**
+	 * Callback to listener on add tile to scene event
+	 * @param node The node added to the scene
+	 */
 	public void onAddToScene(Node node);
+	/**
+	 * Callback to listener on remove tile from scene event
+	 * @param node The node removed from the scene
+	 */
 	public void onRemoveFromScene(Node node);
 }
