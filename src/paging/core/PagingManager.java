2e1bdc7c13b51d0ee222d9fbcd7914b3db72fffd
cristofflanders@gmail.com <cristofflanders@gmail.com@e932bb26-a792-46c9-b5dd-823ed31c32f9>
2012-12-18 19:01:44 +0000

    Finished basic commenting

--- src/paging/core/PagingManager.java
+++ src/paging/core/PagingManager.java
@@ -34,31 +34,54 @@ public class PagingManager {
 	private PhysicsSpace physics = null;
 	
 	protected HashMap<String, Delegator> delegators = new HashMap();
-	
+	/**
+	 * Creates a new instance of the PagingManager
+	 * @param exec The ScheduledThreadPoolExecutor used by the user's application
+	 * @param cam The default camera used by the scene
+	 */
 	public PagingManager(ScheduledThreadPoolExecutor exec, Camera cam) {
 		this.exec = exec;
 		this.cam = cam;
 	}
-	
+	/**
+	 * Enable support for basic physics
+	 * @param physics The PhysicsSpace used by the user's application
+	 */
 	public void addPhysicsSupport(PhysicsSpace physics) {
 		this.managePhysics = true;
 		this.physics = physics;
 	}
-	
+	/**
+	 * Returns if the PagingManager supports physics management
+	 * @return 
+	 */
 	public boolean getManagePhysics() {
 		return this.managePhysics;
 	}
-	
+	/**
+	 * Returns the registered PhysicsSpace
+	 * @return 
+	 */
 	public PhysicsSpace getPhysicsSpace() {
 		return this.physics;
 	}
-	
+	/**
+	 * Registers a delegator with the PagingManager
+	 * @param UID The unique String friendly name of the delegator (i.e. Terrain, Vegeation, etc)
+	 * @param delegator The Delegator to register
+	 * @param spatial The spatial to apply the delegator to
+	 * @param cacheSize The cache limit of the delegator
+	 */
 	public void registerDelegator(String UID, Delegator delegator, Spatial spatial, int cacheSize) {
 		delegator.initDelegator(UID, exec, spatial, cam, this, cacheSize);
 		delegators.put(UID, delegator);
 		spatial.addControl(delegator);
 	}
-	
+	/**
+	 * Returns the delegator registered with the specified UID
+	 * @param UID The unique String friendly name of the delegator
+	 * @return 
+	 */
 	public Delegator getDelegatorByUID(String UID) {
 		return delegators.get(UID);
 	}
